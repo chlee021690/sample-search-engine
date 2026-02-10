255ce689a162b511e315ad5bc0fe700ffbe80deb
chlee021690 <chlee0216@alumni.upenn.edu>
2026-02-10 20:55:33 +0900

    #4: modified a configuration

--- src/main/java/com/chlee021690/sampleproject/searchengine/config/ElasticSearchConfig.java
+++ src/main/java/com/chlee021690/sampleproject/searchengine/config/ElasticSearchConfig.java
@@ -13,16 +13,42 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Value("${spring.elasticsearch.uris}")
     private String esHost;
 
+    @Value("${spring.elasticsearch.username}")
+    private String esUsername;
+
+    @Value("${spring.elasticsearch.password}")
+    private String esPassword;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         URI uri = URI.create(esHost);
 
-        String[] userInfo = uri.getUserInfo().split(":");
+        String[] userInfo = uri.getUserInfo() != null ? uri.getUserInfo().split(":", 2) : new String[0];
+        String username = (esUsername != null && !esUsername.isBlank())
+            ? esUsername
+            : (userInfo.length > 0 ? userInfo[0] : null);
+        String password = (esPassword != null && !esPassword.isBlank())
+            ? esPassword
+            : (userInfo.length > 1 ? userInfo[1] : null);
 
-        return ClientConfiguration.builder()
-                .connectedTo(uri.getHost() + ":" + uri.getPort())
-                .withBasicAuth(userInfo[0], userInfo[1])
-                .build();
+        int port = uri.getPort();
+        if (port == -1) {
+            port = "https".equalsIgnoreCase(uri.getScheme()) ? 443 : 9200;
+        }
+
+        var baseBuilder = ClientConfiguration.builder()
+            .connectedTo(uri.getHost() + ":" + port);
+
+        if ("https".equalsIgnoreCase(uri.getScheme())) {
+            var secureBuilder = baseBuilder.usingSsl();
+            return (username != null && password != null)
+                ? secureBuilder.withBasicAuth(username, password).build()
+                : secureBuilder.build();
+        }
+
+        return (username != null && password != null)
+            ? baseBuilder.withBasicAuth(username, password).build()
+            : baseBuilder.build();
     }
 
 }
