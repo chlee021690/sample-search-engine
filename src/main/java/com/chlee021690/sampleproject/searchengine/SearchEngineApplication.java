5a6d5076ae4058151cf18d433b93f2157a61fa90
chlee021690 <chlee0216@alumni.upenn.edu>
2026-02-13 22:12:04 +0900

    added annotation

--- src/main/java/com/chlee021690/sampleproject/searchengine/SearchEngineApplication.java
+++ src/main/java/com/chlee021690/sampleproject/searchengine/SearchEngineApplication.java
@@ -2,8 +2,10 @@ package com.chlee021690.sampleproject.searchengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories(basePackages = "com.chlee021690.sampleproject.searchengine.repository", repositoryImplementationPostfix = "Impl")
 public class SearchEngineApplication {
 
 	public static void main(String[] args) {
